6cc0152a558d1fef434fec0c6c3b58dd542e2320
EmilySianez-Tec2 <L26550252@chihuahua2.TECNM.MX>
2026-09-03 18:49:29 -0600

    Add files via upload

---
+++ EVA1_2_HOLA_MUNDO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_hola_mundo;
+
+/**
+ *
+ * @author emilycelestesianezgarcia
+ */
+public class EVA1_2_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//INICIO DE PROGRAMA
+        // //ESTO ES UN BLOQUE DE CODIGO TODO
+        //MOSTRAR UN MENSAJE DE TEXTO
+        //MOSTRAR --> INSTRUCCION
+        //TEXTO --> DATOS
+        System.out.println("Hola mundo!!");
+        
+        
+    }//FIN DEL PROGRAMA
+    
+}
---
+++ EVA1_3_Salidas.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author emilycelestesianezgarcia
+ */
+public class EVA1_3_Salidas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Fundamentos de Programacion");
+        System.out.println("Emily Celeste Siañez Garcia");
+        System.out.print("Tu calificacion es: ");
+        System.out.println(85);
+    }
+    
+}
